b395e923ccf432360f87aeaf4dbfb111e203bf55
Roald Ong <roald.ong@gmail.com>
2022-05-26 23:58:32 +0800

    Added in dashes in a row

--- src/App.js
+++ src/App.js
@@ -1,55 +1,53 @@
 import "./App.css";
-import Header from './Components/Header'
-import Figure from './Components/Figure'
-import React, { useState, useEffect } from 'react'
-import Word from './Components/Word'
-import WrongLetters from './Components/WrongLetters';
-const wordList = ['singapore', 'tiktok', 'camp', 'youth']
-let selectedWord = wordList[Math.floor(Math.random() * wordList.length)]
+import Header from "./Components/Header";
+import Figure from "./Components/Figure";
+import React, {useState, useEffect} from "react";
+import Word from "./Components/Word";
+import WrongLetters from "./Components/WrongLetters";
+const wordList = ["singapore", "tiktok", "camp", "youth"];
+let selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
 function App() {
-    
-    const [correctLetters, setCorrectLetters] = useState([])
-    const [playable, setPlayable] = useState(true)
-    const [wrongLetters, setWrongLetters] = useState([])
+	const [correctLetters, setCorrectLetters] = useState([]);
+	const [playable, setPlayable] = useState(true);
+	const [wrongLetters, setWrongLetters] = useState([]);
 
-    useEffect(() => {
-      
-      const handleFormSubmit = (event) => {
-       const letter = event.key;
-	   if (wrongLetters.length === selectedWord.length) {
-		   setPlayable(value => [false]);
-		   alert("Game Over! Try Again.");
-	   }         
-	   if (correctLetters.length === selectedWord.length) {
-		alert("You Won!");
-	   }
-          if (playable && event.keyCode >= 65 && event.keyCode <= 90) {
-          if (selectedWord.includes(letter)) {
-            if (!correctLetters.includes(letter)) {
-              setCorrectLetters(currentLetters => [...currentLetters, letter]);
-            } 
-          } else {
-            if (!wrongLetters.includes(letter)) {
-              setWrongLetters(wrongLetters => [...wrongLetters, letter]);
-            } 
-          }
-        }
-	}
-        
-        //const button = document.querySelector('button')
-       
-      window.addEventListener('keydown', handleFormSubmit); 
-     
-      return () => window.removeEventListener('keydown', handleFormSubmit);
-      }, [correctLetters, wrongLetters, playable]);
-    
-  return (    
-    <>
-  <Header />
-        <Figure wrongLetters={wrongLetters}/>
-        <WrongLetters wrongLetters={wrongLetters} />
-        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
-  </>
-  )
+	useEffect(() => {
+		const handleFormSubmit = event => {
+			const letter = event.key;
+			if (wrongLetters.length === selectedWord.length) {
+				setPlayable(value => [false]);
+				alert("Game Over! Try Again.");
+			}
+			if (correctLetters.length === selectedWord.length) {
+				alert("You Won!");
+			}
+			if (playable && event.keyCode >= 65 && event.keyCode <= 90) {
+				if (selectedWord.includes(letter)) {
+					if (!correctLetters.includes(letter)) {
+						setCorrectLetters(currentLetters => [...currentLetters, letter]);
+					}
+				} else {
+					if (!wrongLetters.includes(letter)) {
+						setWrongLetters(wrongLetters => [...wrongLetters, letter]);
+					}
+				}
+			}
+		};
+
+		//const button = document.querySelector('button')
+
+		window.addEventListener("keydown", handleFormSubmit);
+
+		return () => window.removeEventListener("keydown", handleFormSubmit);
+	}, [correctLetters, wrongLetters, playable]);
+
+	return (
+		<>
+			<Header />
+			<Figure wrongLetters={wrongLetters} />
+			<WrongLetters wrongLetters={wrongLetters} />
+			<Word selectedWord={selectedWord} correctLetters={correctLetters} />
+		</>
+	);
 }
 export default App;
--- src/Components/Word.js
+++ src/Components/Word.js
@@ -1,17 +1,21 @@
 import React from "react";
 
+import "../index.css";
+
 const Word = props => {
 	console.log("selected word " + props.selectedWord);
 	console.log("correctLetters" + props.correctLetters);
 	const numbers = [1, 2, 3, 4, 5];
 	let splitCorrect = props.selectedWord.split("");
 	console.log(splitCorrect.length);
-	const wow = numbers.map(x => <div>"_"</div>);
-	return (
-		<div className="Word">
-			<ul>{wow}</ul>
+	const wow = splitCorrect.map(inp => (
+		<div className="colC">
+			<h2>_</h2>
 		</div>
-	);
+	));
+
+	<div className="rowC">{wow}</div>;
+	return <div>{wow}</div>;
 };
 
 export default Word;
