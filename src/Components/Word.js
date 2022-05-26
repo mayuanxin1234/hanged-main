8d0aa4d0fc5cf3b0af785983b216d84e399a1c53
Roald Ong <roald.ong@gmail.com>
2022-05-26 23:26:24 +0800

    initial test code

--- src/Components/Word.js
+++ src/Components/Word.js
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Word = ({selectedWord, correctLetters}) => {
-  return (
-      <div className = "Word">
-          {selectedWord.split('').map((letter, i) => {
-              return (
-                <span className = "letter" key = {i}>
-                    {correctLetters.includes(letter) ? letter : ''}
-                </span>
-              )
-          })}  
-      </div>
-  
-  )
-}
+const Word = props => {
+	console.log("selected word " + props.selectedWord);
+	console.log("correctLetters" + props.correctLetters);
+	const numbers = [1, 2, 3, 4, 5];
+	let splitCorrect = props.selectedWord.split("");
+	console.log(splitCorrect.length);
+	const wow = numbers.map(x => <div>"_"</div>);
+	return (
+		<div className="Word">
+			<ul>{wow}</ul>
+		</div>
+	);
+};
 
-export default Word
+export default Word;
