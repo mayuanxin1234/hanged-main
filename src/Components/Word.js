0cf55212ce15b44a1a0fc5c9bdc503e64499b675
Roald Ong <roald.ong@gmail.com>
2022-05-27 22:10:45 +0800

    initial working version

--- src/Components/Word.js
+++ src/Components/Word.js
@@ -4,13 +4,13 @@ import "../index.css";
 
 const Word = props => {
 	console.log("selected word " + props.selectedWord);
-	console.log("correctLetters" + props.correctLetters);
+	console.log(props.correctLetters);
 	let splitCorrect = props.selectedWord.split("");
 	console.log(splitCorrect.length);
 	const wow = splitCorrect.map(inp => (
 		<div className="colC">
 			<div className="word">
-				<h1>{inp}</h1>
+				<h1>{props.correctLetters.includes(inp) ? inp : ""}</h1>
 			</div>
 		</div>
 	));
