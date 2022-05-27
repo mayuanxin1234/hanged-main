e7936b1732e41b4cc9ae08fd8e69f653fa19308a
Roald Ong <roald.ong@gmail.com>
2022-05-27 22:04:27 +0800

    fixed letter shifting

--- src/Components/Word.js
+++ src/Components/Word.js
@@ -5,12 +5,13 @@ import "../index.css";
 const Word = props => {
 	console.log("selected word " + props.selectedWord);
 	console.log("correctLetters" + props.correctLetters);
-	const numbers = [1, 2, 3, 4, 5];
 	let splitCorrect = props.selectedWord.split("");
 	console.log(splitCorrect.length);
 	const wow = splitCorrect.map(inp => (
 		<div className="colC">
-			<h2>_</h2>
+			<div className="word">
+				<h1>{inp}</h1>
+			</div>
 		</div>
 	));
 
